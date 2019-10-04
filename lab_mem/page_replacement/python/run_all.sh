7daef827ccccb4806b89b8145fe8e4a40d0f4260
Caio Felipe de Alcantara Melo 115110593 <caiofelipee17@gmail.com>
2019-10-04 11:56:39 -0300

    Add plots

--- lab_mem/page_replacement/python/run_all.sh
+++ lab_mem/page_replacement/python/run_all.sh
@@ -6,11 +6,11 @@ clock=200
 
 for trace in trace.1 trace.2
 do
-    for alg in fifo lru nru aging second-chance
+    for alg in fifo lru nru aging second-chance random
     do
         for nframes in 4 8 16 32
         do
-	    mkdir output/$trace
+	    mkdir -p output/$trace
 	    python memory_simulation.py $npages $nframes $alg $clock < load/$trace.in > output/$trace/$trace.$alg.$nframes.out
 	done
     done
